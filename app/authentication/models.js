94fd90005e5c8fa0e918a55c92d1af0307efd762
drxos <sounton@gmail.com>
2017-06-10 14:00:22 +0100

    auth: yes

--- app/authentication/models.js
+++ app/authentication/models.js
@@ -31,8 +31,4 @@ userSchema.methods.generateHash = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
 
-userSchema.methods.validPassword = (password) => {
-  return bcrypt.compareSync(password, this.local.password)
-}
-
 module.exports = mongoose.model('User', userSchema)
