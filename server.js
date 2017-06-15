e164a44313cad0958d978781e7ccfc4732c4da24
drxos <sounton@gmail.com>
2017-06-15 08:43:34 +0100

    ipr: add error page

--- server.js
+++ server.js
@@ -18,27 +18,11 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
+  console.log(req.app.get('env'));
   res.redirect('/user/list')
 })
 
 app.use(auth)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error');
-});
 
 module.exports = app
